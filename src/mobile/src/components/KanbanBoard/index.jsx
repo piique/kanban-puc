feef3cae11725b18af3442de230d79e978c0170d
Pedro Vilaça <pedrovilacav@gmail.com>
2023-04-26 15:18:41 -0300

    Correção e ajustes

--- src/mobile/src/components/KanbanBoard/index.jsx
+++ src/mobile/src/components/KanbanBoard/index.jsx
@@ -1,50 +1,18 @@
-/* eslint-disable no-unused-vars */
 import { useCallback, useEffect, useMemo, useState } from "react";
 import TaskFormModal from "../TaskFormModal";
 import "./styles.css"
 import TaskCard from "../TaskCard";
-import { getAllTasks, insertTask } from "../../api";
+import { getAllTasks, insertTask, updateTaskById } from "../../api";
 
 const KanbanBoard = () => {
 
     const [showModal, setShowModal] = useState(false);
 
-    const [tasks, setTasks] = useState([
-        {
-            id: 1,
-            title: "Tarefa 1",
-            description: "Descrição da tarefa 1",
-            pomodoroCount: 1,
-            status: 0,
-        },
-        {
-            id: 2,
-            title: "Tarefa 1",
-            description: "Descrição da tarefa 1",
-            pomodoroCount: 1,
-            status: 1,
-        },
-        {
-            id: 3,
-            title: "Tarefa 1",
-            description: "Descrição da tarefa 1",
-            pomodoroCount: 1,
-            status: 2,
-        },
-        {
-            id: 4,
-            title: "Tarefa 1",
-            description: "Descrição da tarefa 1",
-            pomodoroCount: 1,
-            status: 1,
-        },
-    ]);
+    const [tasks, setTasks] = useState([]);
 
     useEffect(() => {
         getAllTasks()
             .then((response) => {
-                console.log('response', response);
-                console.log("Tarefas carregadas com sucesso");
                 const _tasks = response.data.map((task) => ({
                     id: task.id,
                     title: task.name,
@@ -52,7 +20,6 @@ const KanbanBoard = () => {
                     pomodoroCount: task.estimated_pomodoros,
                     status: task.status,
                 }));
-                console.log('_tasks', _tasks);
 
                 setTasks(_tasks);
             })
@@ -61,7 +28,7 @@ const KanbanBoard = () => {
                 console.log(error);
             })
             .finally(() => {
-                console.log("Finalizando a requisição");
+                // console.log("Finalizando a requisição");
             });
     }, []);
 
@@ -81,7 +48,7 @@ const KanbanBoard = () => {
             .then((response) => {
                 console.log('response', response);
                 console.log("Tarefa salva com sucesso");
-                setTasks(prevState => [...prevState, { id:response.data.id , title, description, pomodoroCount, status: Number(status) }]);
+                setTasks(prevState => [...prevState, { id: response.data.id, title, description, pomodoroCount, status: Number(status) }]);
             })
             .catch((error) => {
                 console.log("Ocorreu um erro ao salvar a tarefa");
@@ -100,26 +67,52 @@ const KanbanBoard = () => {
 
     const tasksDone = useMemo(() => tasks.filter((task) => task.status === 2), [tasks]);
 
+    const updateStatusTask = useCallback((idTask, newStatus) => {
+        updateTaskById(idTask, { status: newStatus })
+            .then(() => {
+                setTasks((prevState) => {
+                    const newStateTasks = prevState.map((task) => {
+                        if (task.id === idTask) {
+                            return {
+                                ...task,
+                                status: Number(newStatus),
+                            };
+                        }
+                        return task;
+                    });
+                    return newStateTasks;
+                });
+            })
+            .catch((error) => {
+                alert('Não foi possível atualizar a tarefa. Entre em contato com o administrador do sistema!');
+                console.log("Ocorreu um erro ao updatar a tarefa");
+                console.log(error);
+            })
+            .finally(() => {
+                // console.log("Finalizando a requisição");
+            });
+    }, [])
+
     return (
         <>
-            <TaskFormModal showModal={showModal} onClose={() => setShowModal(false)} onSubmit={saveNewTask} />
+            {showModal && <TaskFormModal showModal={showModal} onClose={() => setShowModal(false)} onSubmit={saveNewTask} />}
             <div className="quadro">
                 <div className="coluna">
                     <div className="titulo-coluna">A fazer</div>
                     {tasksToDo.map((task) => (
-                        <TaskCard key={task.id} task={task} />
+                        <TaskCard key={task.id} task={task} updateStatusTask={updateStatusTask} />
                     ))}
                 </div>
                 <div className="coluna">
                     <div className="titulo-coluna">Fazendo</div>
                     {tasksDoing.map((task) => (
-                        <TaskCard key={task.id} task={task} />
+                        <TaskCard key={task.id} task={task} updateStatusTask={updateStatusTask} />
                     ))}
                 </div>
                 <div className="coluna">
                     <div className="titulo-coluna">Concluído</div>
                     {tasksDone.map((task) => (
-                        <TaskCard key={task.id} task={task} />
+                        <TaskCard key={task.id} task={task} updateStatusTask={updateStatusTask} />
                     ))}
                 </div>
                 <button className="botao-adicionar" onClick={() => setShowModal(true)}>Adicionar Tarefa</button>
